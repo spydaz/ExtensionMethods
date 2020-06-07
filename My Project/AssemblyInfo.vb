68007484f327f701db0ba736db0ee467acd127df
SpydaZ <LeroySamuelDyer@hotmail.co.uk>
2020-06-07 18:11:21 +0100

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ExtensionMethods Library")>
+<Assembly: AssemblyDescription("Basic Extension Methods Library")>
+<Assembly: AssemblyCompany("SpydazWeb AI")>
+<Assembly: AssemblyProduct("SpydazWeb AI ExtensionMethods")>
+<Assembly: AssemblyCopyright("SpydazWeb AI Copyright ©  2020")>
+<Assembly: AssemblyTrademark("SpydazWeb AI TM ©  2020")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0b7bc494-0568-4898-8346-4956ae919f7c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
